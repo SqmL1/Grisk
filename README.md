e2f5e8b107bae85d78448d7dc0e10e9b24fb5aea
Samuil Iordachi <samuil.iordachi@vervint.com>
2024-07-01 15:43:03 -0500

    Changed from selenium to playwright, first website access

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 The purpose of this program is to do the following:
 
-1. Web scrape the WSB subreddit using Selenium
+1. Web scrape the WSB subreddit using Playwright
 2. Analyze stock sentiment using NLTK and Pandas
 3. Use flask or Django for the web framework
 4. Allow for a user interface with a GUI
--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import sqlite3
-import selenium
-import nltk
-import flask
+import sqlite3, playwright, nltk, flask, nest_asyncio
+from playwright.sync_api import sync_playwright
+import playwright.sync_api
+
+nest_asyncio.apply()
+pw = sync_playwright().start()
+browser = pw.chromium.launch(headless=False)
+page = browser.new_page()
+page.goto("https://scrapfly.io/blog/web-scraping-with-playwright-and-python/")
+
+
+
