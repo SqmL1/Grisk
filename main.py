5deb97f133b18b4838826383c556aad835f37111
SqmL1 <samuil.iordachi@vervint.com>
2024-07-02 09:39:41 -0500

    function based pw usage

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-import sqlite3, playwright, nltk, flask, nest_asyncio
+import sqlite3, playwright, nltk, flask, nest_asyncio, parsel
 from playwright.sync_api import sync_playwright
 import playwright.sync_api
 
+'''
 nest_asyncio.apply()
 pw = sync_playwright().start()
 browser = pw.chromium.launch(headless=False)
 page = browser.new_page()
 page.goto("https://scrapfly.io/blog/web-scraping-with-playwright-and-python/")
+'''
 
 
-
+with sync_playwright() as pw:
+    browser = pw.chromium.launch(headless=False)
+    context = browser.new_context(viewport={"width": 1920, "height": 1080})
+    page = context.new_page()
+    page.goto("https://scrapfly.io/blog/web-scraping-with-playwright-and-python/")
+    print(page.content())
+    
+    
