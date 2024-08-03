855c074714b0f49c4e0a365362b09667da33e187
sqmL1 <samuiliordachi@gmail.com>
2024-08-03 11:07:42 -0500

    mm

--- main.py
+++ main.py
@@ -18,23 +18,39 @@ def text_to_db(page, url):
         return None
 
 with sync_playwright() as pw:
+    #func sync_plawright() = pw 
     home_page = "https://www.reddit.com/r/wallstreetbets/"
+    #the page the playwright navigates to
     browser = pw.chromium.launch(headless=False, channel="chrome")
+    #opens chrome using pw in headless mode
     context = browser.new_context(viewport={"width": 1920, "height": 1080})
+    #context is gives specifics about the browser opened
     page = context.new_page()
+    #using that context open a new page
     page.goto(home_page)
+    #go to reddit homepage
 
     while True:
+    #boolean operator to always run
         links = page.locator("a")
+        #one the page locate the html anchor elements
 
         for link in links.all():
+        #loop through all the links in the anchor elements
             href = links.get_attribute("href")
 
             with page.context.expect_page() as new_page_info:
                 link.click()
-            new_page = new_page_info.value
+                new_page = new_page_info.value
+        break
 
-            text = text_to_db(new_page, href)
+    browser.close()
+    connection.close()
+
+
+#for later?
+'''
+ text = text_to_db(new_page, href)
             if text:
                 try:
                     cursor.execute("INSERT INTO posts VALUES (?, ?)", (href, text))
@@ -44,9 +60,4 @@ with sync_playwright() as pw:
             
             new_page.close()
             time.sleep(100)
-            
-        break
-
-    browser.close()
-    connection.close()
-
+'''
